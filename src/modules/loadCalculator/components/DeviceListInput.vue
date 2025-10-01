74dee7fea820f615c8255dbab61ffc515dee56a7
Your Name <your.email@example.com>
2025-09-30 23:06:33 -0600

    added interlock for heaters and a/c and some other changes

--- src/modules/loadCalculator/components/DeviceListInput.vue
+++ src/modules/loadCalculator/components/DeviceListInput.vue
@@ -2,30 +2,46 @@
 <template>
   <div>
     <!-- Add Device Buttons -->
-    <q-btn label="Add Water Heater" @click="addDevice('waterHeater')" color="primary" icon="add" class="q-mb-sm" />
-    <q-btn label="Add Range" @click="addDevice('range')" color="secondary" icon="add" class="q-ml-sm q-mb-sm" />
-    <q-btn label="Add EV" @click="addDevice('ev')" color="teal" icon="add" class="q-ml-sm q-mb-sm" />
-    <q-btn label="Add Appliance >1500W" @click="addDevice('appliance')" color="orange" icon="add"
-      class="q-ml-sm q-mb-sm" />
+    <div class="add-buttons q-mb-sm">
+      <q-btn v-for="opt in DEVICE_TYPE_OPTIONS" :key="opt.value" :label="opt.label" :color="opt.color" :icon="opt.icon"
+        class="q-mr-sm q-mb-sm" @click="addDevice(opt.value)" />
+    </div>
+
     <!-- 全局 EVEMS 开关 -->
     <q-toggle v-if="devices.some(d => d.type === 'ev')" v-model="evEmsModel" label="Has EV Energy Management System?"
       class="q-mt-md" />
     <!-- Device List -->
     <div v-for="(device, index) in devices" :key="device.id" class="q-mt-sm">
       <!-- Device Type -->
-      <q-select v-model="device.type" :options="deviceTypes" option-label="label" option-value="value"
-        @blur="onTypeChange(index)" />
+      <q-select v-model="device.type" :options="DEVICE_TYPE_OPTIONS" option-label="label" option-value="value"
+        @update:model-value="onTypeChange(index)" />
+
+      <!-- Heater Subtype -->
+      <template v-if="device.type === 'heater'">
+        <q-select :options="SUB_TYPE_MAP.heater" v-model="device.heaterType" label="Subtype" />
+        <q-input v-model.number="device.kw" label="Value" type="number" />
+      </template>
+
+      <!-- AC Subtype -->
+      <template v-if="device.type === 'ac'">
+        <q-select :options="SUB_TYPE_MAP.ac.styles" v-model="device.style" label="Style"
+          @update:model-value="device.voltage = (device.style === 'window' ? 120 : 240)" />
+        <q-select :options="SUB_TYPE_MAP.ac.units" v-model="device.unit" label="Unit" />
+        <q-input v-model.number="device.value" label="Value" type="number" />
+        <q-input v-if="device.unit === 'current'" v-model.number="device.voltage" label="Voltage (V)" type="number" />
+      </template>
 
       <!-- Water Heater Subtype -->
-      <q-select v-if="device.type === 'waterHeater'" v-model="device.subType" :options="SUB_TYPES.waterHeater"
-        label="Subtype" />
-
+      <template v-if="device.type === 'waterHeater'">
+        <q-select v-model="device.subType" :options="SUB_TYPE_MAP.waterHeater" label="Subtype" />
+        <q-input v-model.number="device.kw" label="Water Heater Power (kW)" type="number" />
+      </template>
       <!-- EV Auto Management -->
       <q-input v-if="device.type === 'ev'" v-model="device.kw" label="EV Charger Rating (kW)" />
 
       <!-- Range Input -->
       <q-input v-if="device.type === 'range'" v-model.number="device.kw" label="Range Rating (kW)" type="number"
-        @blur="organizeRanges" />
+        @update:model-value="organizeRanges" />
 
       <!-- Other Appliance Input -->
       <q-input v-if="device.type === 'appliance'" v-model.number="device.kw" label="Appliance Power > 1500 (W)"
@@ -39,28 +55,14 @@
 
 <script setup lang="ts">
 import { ref, watch } from 'vue'
-import type { Device, DeviceType } from '../../../types/device'
-// import { loadESLint } from 'eslint';
+import { SUB_TYPE_MAP, type DeviceType, type Device, DEVICE_TYPE_OPTIONS } from '../../../types/device'
 
-const SUB_TYPES = {
-  waterHeater: [
-    'electric tankless water heaters',
-    'electric water heaters for steamers',
-    'electric water heaters for swimming pools',
-    'electric water heaters for hot tubs',
-    'electric water heaters for spas'
-  ]
-}
-const deviceTypes: { label: string; value: DeviceType }[] = [
-  { label: 'Water Heater', value: 'waterHeater' },
-  { label: 'Appliance >1500W', value: 'appliance' },
-  { label: 'EV Charger', value: 'ev' },
-  { label: 'Range', value: 'range' }
-]
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   modelValue: Device[]
   hasEvEms: boolean // optional prop to bind EV EMS toggle
-}>()
+}>(), {
+  hasEvEms: false
+})
 
 const emit = defineEmits<{
   (e: "update:modelValue", value: Device[]): void
@@ -75,53 +77,52 @@ const evEmsModel = ref(props.hasEvEms)
 watch(devices, val => emit("update:modelValue", val), { deep: true })
 watch(evEmsModel, val => emit("update:hasEvEms", val))
 
-// const evEmsModel = computed({
-//   get: () => props.hasEvEms,
-//   set: (val: boolean) => emit('update:hasEvEms', val)
-// })
-
-// watch(
-//   () => props.hasEvEms,
-//   (val) => {
-//     if (val !== undefined) {
-//       const updated = devices.value.map(d =>
-//         d.type === "EV" ? { ...d, hasAutoManagement: val } : d
-//       )
-//       devices.value = updated
-//       emit("update:modelValue", updated)
-//     }
-//   }
-// )
-// const devices = computed({
-//   get: () => props.modelValue,
-//   set: (val) => emit('update:modelValue', val)
-// })
-
 function addDevice(type: DeviceType) {
   const id = crypto.randomUUID()
   let newDevice: Device
 
   switch (type) {
     case 'ev':
-      newDevice = { id, name: '', type, kw: 7.2, hasAutoManagement: evEmsModel.value }
+      newDevice = { id, name: '', type, kw: 0, hasAutoManagement: evEmsModel.value }
       break
     case 'range':
-      newDevice = { id, name: '', type, kw: 12 }
+      newDevice = { id, name: '', type, kw: 0 }
       break
     case 'waterHeater':
       newDevice = {
         id,
         name: '',
         type,
-        kw: 4.5,
-        subType: SUB_TYPES.waterHeater[0]
+        kw: 0,
+        subType: SUB_TYPE_MAP.waterHeater[0]
       }
       break
+    case 'heater':
+      newDevice = {
+        id,
+        name: '',
+        type,
+        kw: 0,
+        heaterType: SUB_TYPE_MAP.heater[0]
+      }
+      break
+    case 'ac':
+      newDevice = {
+        id,
+        name: '',
+        type,
+        value: 0,
+        unit: SUB_TYPE_MAP.ac.units[0],
+        style: SUB_TYPE_MAP.ac.styles[0],
+        voltage: 240
+      }
+      break
+
     case 'appliance':
-      newDevice = { id, name: '', type, kw: 2 }
+      newDevice = { id, name: '', type, kw: 0 }
       break
     default:
-      throw new Error(`Unsupported device type: ${type}`)
+      throw new Error(`Unsupported device type: ${String(type)}`)
   }
 
   devices.value = [...devices.value, newDevice]
@@ -139,7 +140,9 @@ function onTypeChange(index: number) {
   const device = updated[index]
   if (!device) return
   if (device.type === 'waterHeater' && !device.subType) {
-    device.subType = SUB_TYPES.waterHeater[0] as string
+    device.subType = SUB_TYPE_MAP.waterHeater[0]
+  } else if (device.type === 'heater' && !device.heaterType) {
+    device.heaterType = SUB_TYPE_MAP.heater[0]
   }
 
   devices.value = updated
